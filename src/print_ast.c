4f3161e21fcb3d7d110157584fd411b9f960cd3b
justm <tanausti07@gmail.com>
2026-06-07 21:10:01 +0000

    print_ast prints to file and not just stdout

--- src/print_ast.c
+++ src/print_ast.c
@@ -5,44 +5,45 @@
 #include "print_ast.h"
 
 
-void print_ast(ast_node_t ast){
+void print_ast(FILE* out, ast_node_t ast){
+
 
 
 	switch(ast.type){
 
 		case AST_PROGRAM:
 			{
-				print_program_node(ast.as.program, 0);
+				print_program_node(out, ast.as.program, 0);
 				break;
 			}
 		case AST_FUNCTION:
 			{
-				print_function_node(ast.as.function, 2);	
+				print_function_node(out, ast.as.function, 2);	
 				break;
 			}
 		case AST_STATEMENT:
 			{
-				print_statement_node(ast.as.statement, 4);
+				print_statement_node(out, ast.as.statement, 4);
 				break;
 			}
 		case AST_BINARY_EXPRESSION:
 			{
-				print_binary_expression_node(ast.as.binary_expression, 5);
+				print_binary_expression_node(out, ast.as.binary_expression, 5);
 				break;
 			}
 		case AST_KEYWORD:
 			{
-				print_keyword_node(ast.as.keyword, 5);
+				print_keyword_node(out, ast.as.keyword, 5);
 				break;
 			}
 		case AST_PRIMARY:
 			{
-				print_primary_node(ast.as.primary, 6);
+				print_primary_node(out, ast.as.primary, 6);
 				break;
 			}
 		default:
 			{
-				printf("Invalid token\n");
+				fprintf(out, "Invalid token\n");
 				break;
 			}
 
@@ -53,14 +54,14 @@ void print_ast(ast_node_t ast){
 
 
 
-void indent(int indent_level){
+void indent(FILE* out, int indent_level){
 
 
 	
 	for(int i = 0; i < indent_level; i++){
 
 
-		printf("\t");
+		fprintf(out, "\t");
 
 
 	}
@@ -71,48 +72,48 @@ void indent(int indent_level){
 
 
 
-void print_program_node(program_t program, int indent_level){
+void print_program_node(FILE* out, program_t program, int indent_level){
 
-	indent(indent_level);
-	printf("-program:\n");
+	indent(out, indent_level);
+	fprintf(out, "-program:\n");
 
-	print_function_list(program.function_list, indent_level + 1);
+	print_function_list(out, program.function_list, indent_level + 1);
 
 
 }
 
-void print_function_list(function_list_t function_list, int indent_level){
+void print_function_list(FILE* out, function_list_t function_list, int indent_level){
 
-	indent(indent_level);
-	printf("+function_list:\n");
+	indent(out, indent_level);
+	fprintf(out, "+function_list:\n");
 
 	for(int i = 0; i < function_list.vector_tree.size; i++){
-		print_ast(*function_list.vector_tree.children[i]);
+		print_ast(out, *function_list.vector_tree.children[i]);
 	}
 
 
 }
 
 
-void print_function_node(function_t function, int indent_level){
+void print_function_node(FILE* out, function_t function, int indent_level){
 
-	indent(indent_level);
-	printf("-function:\n");
+	indent(out, indent_level);
+	fprintf(out, "-function:\n");
 
 	function_prototype_t function_prototype = function.function_prototype;
-	print_function_prototype(function_prototype, indent_level + 1);
+	print_function_prototype(out, function_prototype, indent_level + 1);
 
 	statement_list_t statement_list = function.statement_list;
-	print_statement_list(statement_list, indent_level + 1);
+	print_statement_list(out, statement_list, indent_level + 1);
 
 
 }
 
 
-void print_function_prototype(function_prototype_t function_prototype, int indent_level){
+void print_function_prototype(FILE* out, function_prototype_t function_prototype, int indent_level){
 
-	indent(indent_level);
-	printf("+function_prototype:\n");
+	indent(out, indent_level);
+	fprintf(out, "+function_prototype:\n");
 
 	char* primitive_type_string = "";
 
@@ -126,27 +127,27 @@ void print_function_prototype(function_prototype_t function_prototype, int inden
 			break;
 	}
 
-	indent(indent_level + 1);
-	printf("+primitive_type: %s\n", primitive_type_string);
+	indent(out, indent_level + 1);
+	fprintf(out, "+primitive_type: %s\n", primitive_type_string);
 
 
-	indent(indent_level + 1);
-	printf("+function_name: %s\n", function_prototype.function_name);
+	indent(out, indent_level + 1);
+	fprintf(out, "+function_name: %s\n", function_prototype.function_name);
 
 
 }
 
 
 
-void print_statement_list(statement_list_t statement_list, int indent_level){
+void print_statement_list(FILE* out, statement_list_t statement_list, int indent_level){
 	
-	indent(indent_level);
+	indent(out, indent_level);
 
-	printf("+statement_list:\n");
+	fprintf(out, "+statement_list:\n");
 
 	for(int i = 0; i < statement_list.vector_tree.size; i++){
 
-		print_ast(*statement_list.vector_tree.children[i]);
+		print_ast(out, *statement_list.vector_tree.children[i]);
 
 	}
 
@@ -154,14 +155,14 @@ void print_statement_list(statement_list_t statement_list, int indent_level){
 }
 
 
-void print_statement_node(statement_t statement, int indent_level){
+void print_statement_node(FILE* out, statement_t statement, int indent_level){
 
-	indent(indent_level);
+	indent(out, indent_level);
 
-	printf("-statement:\n");
+	fprintf(out, "-statement:\n");
 
 	for(int i = 0; i < statement.vector_tree.size; i++){
-		print_ast(*statement.vector_tree.children[i]);
+		print_ast(out, *statement.vector_tree.children[i]);
 	}
 
 
@@ -170,30 +171,30 @@ void print_statement_node(statement_t statement, int indent_level){
 
 
 
-void print_binary_expression_node(binary_expression_t binary_expression, int indent_level){
+void print_binary_expression_node(FILE* out, binary_expression_t binary_expression, int indent_level){
 
-	indent(indent_level);
-	printf("-binary_expression:\n");
+	indent(out, indent_level);
+	fprintf(out, "-binary_expression:\n");
 
 
-	indent(indent_level + 1);
-	printf("+primitive_type: ");
+	indent(out, indent_level + 1);
+	fprintf(out, "+primitive_type: ");
 	switch(binary_expression.primitive_type){
 
 		case PRIMITIVE_INT:
-			printf("int\n");
+			fprintf(out, "int\n");
 			break;
 		default:
-			printf("invalid primitive type enum. Enum value: %d\n", binary_expression.primitive_type);
+			fprintf(out, "invalid primitive type enum. Enum value: %d\n", binary_expression.primitive_type);
 			break;
 
 	}
 
-	indent(indent_level + 1);
-	printf("+operator: %c\n", binary_expression.operator);
+	indent(out, indent_level + 1);
+	fprintf(out, "+operator: %c\n", binary_expression.operator);
 
-	print_ast(*binary_expression.left);
-	print_ast(*binary_expression.right);
+	print_ast(out, *binary_expression.left);
+	print_ast(out, *binary_expression.right);
 
 
 }
@@ -201,23 +202,23 @@ void print_binary_expression_node(binary_expression_t binary_expression, int ind
 
 
 
-void print_keyword_node(keyword_t keyword, int indent_level){
+void print_keyword_node(FILE* out, keyword_t keyword, int indent_level){
 
-	indent(indent_level);
-	printf("-keyword:\n");
+	indent(out, indent_level);
+	fprintf(out, "-keyword:\n");
 
 
-	indent(indent_level + 1);
+	indent(out, indent_level + 1);
 	switch(keyword){
 
 		case KEYW_RETURN:
-			printf("keyword: return\n");
+			fprintf(out, "keyword: return\n");
 			break;
 		case KEYW_INT:
-			printf("keyword: int\n");
+			fprintf(out, "keyword: int\n");
 			break;
 		default:
-			printf("invalid keyword enum. Enum value: %d\n", keyword);
+			fprintf(out, "invalid keyword enum. Enum value: %d\n", keyword);
 			break;
 
 	}
@@ -229,22 +230,22 @@ void print_keyword_node(keyword_t keyword, int indent_level){
 
 
 
-void print_primary_node(primary_t primary, int indent_level){
+void print_primary_node(FILE* out, primary_t primary, int indent_level){
 
-	indent(indent_level);
-	printf("-primary:\n");
+	indent(out, indent_level);
+	fprintf(out, "-primary:\n");
 
 
 	switch(primary.type){
 
 		case PRIMARY_LITERAL:
 			{
-				print_literal(primary.as.literal, indent_level + 1);
+				print_literal(out, primary.as.literal, indent_level + 1);
 				break;
 			}
 		case PRIMARY_FUNC_CALL:
 			{
-				print_func_call(primary.as.func_call, indent_level + 1);
+				print_func_call(out, primary.as.func_call, indent_level + 1);
 				break;
 			}
 
@@ -259,21 +260,21 @@ void print_primary_node(primary_t primary, int indent_level){
 
 
 
-void print_literal(literal_t literal, int indent_level){
+void print_literal(FILE* out, literal_t literal, int indent_level){
 
-	indent(indent_level);
-	printf("+literal:\n");
+	indent(out, indent_level);
+	fprintf(out, "+literal:\n");
 
 
 
-	indent(indent_level + 1);
+	indent(out, indent_level + 1);
 	switch(literal.primitive_type){
 
 		case PRIMITIVE_INT:
-			printf("+integer: %d\n", literal.as.integer);
+			fprintf(out, "+integer: %d\n", literal.as.integer);
 			break;
 		default:
-			printf("invalid primitive type enum. Enum value: %d\n", literal.primitive_type);
+			fprintf(out, "invalid primitive type enum. Enum value: %d\n", literal.primitive_type);
 			break;
 
 	}
@@ -283,27 +284,27 @@ void print_literal(literal_t literal, int indent_level){
 
 
 
-void print_func_call(func_call_t func_call, int indent_level){
+void print_func_call(FILE* out, func_call_t func_call, int indent_level){
 
-	indent(indent_level);
-	printf("+func_call:\n");
+	indent(out, indent_level);
+	fprintf(out, "+func_call:\n");
 
 
-	indent(indent_level + 1);
+	indent(out, indent_level + 1);
 	switch(func_call.primitive_type){
 
 		case PRIMITIVE_INT:
-			printf("+primtive_type: int\n");
+			fprintf(out, "+primtive_type: int\n");
 			break;
 		default:
-			printf("invalid primitive type enum. Enum value: %d\n", func_call.primitive_type);
+			fprintf(out, "invalid primitive type enum. Enum value: %d\n", func_call.primitive_type);
 			break;
 
 
 	}
 
-	indent(indent_level + 1);
-	printf("+callee %s\n:", func_call.callee);
+	indent(out, indent_level + 1);
+	fprintf(out, "+callee %s\n:", func_call.callee);
 
 }
 
--- src/print_ast.h
+++ src/print_ast.h
@@ -6,20 +6,20 @@
 #include "parser.h"
 
 
-void print_ast(ast_node_t ast);
-void indent(int indent_level);
+void print_ast(FILE* out, ast_node_t ast);
+void indent(FILE* out, int indent_level);
 
-void print_program_node(program_t program, int indent_level);
-void print_function_list(function_list_t function_list, int indent_level);
-void print_function_node(function_t function, int indent_level);
-void print_function_prototype(function_prototype_t function_prototype, int indent_level);
-void print_statement_list(statement_list_t statement_list, int indent_level);
-void print_statement_node(statement_t statement, int indent_level);
-void print_binary_expression_node(binary_expression_t binary_expression, int indent_level);
-void print_keyword_node(keyword_t keyword, int indent_level);
-void print_primary_node(primary_t primary, int indent_level);
-void print_literal(literal_t literal, int indent_level);
-void print_func_call(func_call_t func_call, int indent_level);
+void print_program_node(FILE* out, program_t program, int indent_level);
+void print_function_list(FILE* out, function_list_t function_list, int indent_level);
+void print_function_node(FILE* out, function_t function, int indent_level);
+void print_function_prototype(FILE* out, function_prototype_t function_prototype, int indent_level);
+void print_statement_list(FILE* out, statement_list_t statement_list, int indent_level);
+void print_statement_node(FILE* out, statement_t statement, int indent_level);
+void print_binary_expression_node(FILE* out, binary_expression_t binary_expression, int indent_level);
+void print_keyword_node(FILE* out, keyword_t keyword, int indent_level);
+void print_primary_node(FILE* out, primary_t primary, int indent_level);
+void print_literal(FILE* out, literal_t literal, int indent_level);
+void print_func_call(FILE* out, func_call_t func_call, int indent_level);
 
 
 
